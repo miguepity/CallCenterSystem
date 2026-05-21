1148aaf347a178c6e49753d8e9a8eb9f6f0e10ef
RoyGeova07 <royumana3@gmail.com>
2026-05-20 19:52:12 -0600

    hecho la tabla call_queue

--- callCenterSystem-API/config/config.js
+++ callCenterSystem-API/config/config.js
@@ -1,23 +1,15 @@
-module.exports = {
+require("dotenv").config()
+const{DB_USER,DB_PASSWORD,DB_NAME}=process.env
+
+module.exports = 
+{
+
   development: {
-    "username": "root",
-    "password": null,
-    "database": "callcenter_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: "127.0.0.1",
+    dialect: "postgres"
   }
+  
 }
---
+++ callCenterSystem-API/models/employees.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class employees extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  employees.init({
+    name: DataTypes.STRING,
+    is_available: DataTypes.BOOLEAN,
+    rank: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'employees',
+    underscored: true,
+  });
+  return employees;
+};
