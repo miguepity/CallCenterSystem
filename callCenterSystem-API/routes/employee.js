f60a4118d9b0c207a8bb9993ecba833823d4278a
RoyGeova07 <royumana3@gmail.com>
2026-05-27 19:03:16 -0600

    Hecho el endpoint get employee

--- callCenterSystem-API/routes/employee.js
+++ callCenterSystem-API/routes/employee.js
@@ -1,11 +1,11 @@
 const router=require('express').Router()
 
-const{createEmployee,desactivarEmployee,activarEmpleado}=require('../controllers/Employee')
+const{createEmployee,desactivarEmployee,activarEmpleado,getEmployee}=require('../controllers/Employee')
 
 router.post("/employee",createEmployee)
 router.put('/employee/desactivate/:id',desactivarEmployee)
 router.put('/employee/activate/:id',activarEmpleado)
-//router.get('/employee',getEmployee)
+router.get('/employee',getEmployee)
 
 
 module.exports=router
