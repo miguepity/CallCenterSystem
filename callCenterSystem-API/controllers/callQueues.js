c8f3b7df9442c11dc5dd3998468e4fada3c56a0e
D4-CTM <josuedelcid325@gmail.com>
2026-05-28 19:41:06 -0600

    Add delete call-queue endpoint

--- callCenterSystem-API/controllers/callQueues.js
+++ callCenterSystem-API/controllers/callQueues.js
@@ -61,8 +61,24 @@ async function getCallQueue(req, res) {
     return res.status(200).json({ message: 'Queue found!', body: queue });
 }
 
+async function deleteCallQueue(req, res) {
+    const { queueId } = req.params;
+
+    if (!queueId)
+        return res.status(400).json({message: 'Please pass the queue id as route parameter'});
+
+    const queue = await db['call_queue'].findByPk(queueId);
+
+    if (!queue)
+        return res.status(404).json({message: `Not found any call queue with id: ${queueId}`});
+
+    await queue.destroy();
+    return res.status(200).json({ message: 'Queue deleted!', body: queue });
+}
+
 module.exports = {
     postCallQueue,
     getCallQueue,
-    putCallQueue
+    putCallQueue,
+    deleteCallQueue
 }
