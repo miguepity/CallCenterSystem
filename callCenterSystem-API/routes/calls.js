4da1603424fa6b807940348533751d1ba6bfc7cf
Jonny Gomez <jonnygomez@unitec.edu>
2026-06-01 16:19:34 -0600

    Add get calls endpoints

--- callCenterSystem-API/routes/calls.js
+++ callCenterSystem-API/routes/calls.js
@@ -3,5 +3,7 @@ var router = express.Router();
 var callsController = require('../controllers/calls');
 
 router.post('/calls', callsController.createcall);
+router.get('/calls', callsController.getcalls);
+router.get('/calls/:id', callsController.getcallbyid);
 
 module.exports = router;
