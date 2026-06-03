20b940d9be63e5b2efb45a26de281c248b068aa4
hectorc10 <hectorcarranza509@gmail.com>
2026-06-02 19:37:11 -0600

    Swagger endpoints employees

--- callCenterSystem-API/routes/employees.js
+++ callCenterSystem-API/routes/employees.js
@@ -6,6 +6,10 @@ router.get('/', employeesController.getEmployees);
 router.get('/:id', employeesController.getEmployeeById);
 router.post('/', employeesController.createEmployee);
 router.delete('/:id', employeesController.deleteEmployee);
+router.put('/:id', employeesController.updateEmployee);
+router.patch('/:id/availability', employeesController.updateAvailability);
+router.get('/:id/calls', employeesController.getEmployeeCalls);
+router.get('/:id/stats', employeesController.getEmployeeStats);
 
 
 module.exports = router;
